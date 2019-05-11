682aedb464bf52f6ad8faff243f86a5dbabe2774
423april <423april@naver.com>
2019-05-12 08:31:29 +0900

    Add files via upload

---
+++ subfinal-수민/TreeNodes (2).h
@@ -0,0 +1,48 @@
+#ifndef TREENODES_H_INCLUDED
+#define TREENODES_H_INCLUDED
+
+#include <iostream>
+#include "ConnectFourBoard (2).h"
+using namespace std;
+
+extern int num_nodes_generated,num_nodes_expanded,game_path_length,start_time,stop_time;
+
+class tree{
+    
+public:
+    
+    //stores the heuristic value of the node i.e board
+    int heuristic_value;
+    /*to iterate through the children of a node and
+     there are exactly 7 possible children for each node
+     */
+    int num_children;
+    //this tores the pointers to its 7 children nodes
+    tree *children[7];
+    
+    bool use;
+    /*an object from the class tic which holds the
+     board configuration of each node
+     */
+    tic *ob;
+    
+    
+    //Member functions of tree class
+    tree();
+    char ReversePlayer(char player);
+    void create_node(char);
+    void set_heuristic_value(int);
+    void add_all_children();
+    bool deep_enough(int);
+    int lhsdiag(char);
+    int rhsdiag(char);
+    int horizontal(char);
+    int vertical(char);
+    int evaluation_m(char);
+    void copy_board_status(tic &);
+    void helper();
+    void display_contents();
+    int getOptimalNode();
+    void move_gen(tic *,int);
+};
+#endif // TREENODES_H_INCLUDED
